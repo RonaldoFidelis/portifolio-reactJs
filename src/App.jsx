9cde018899e29efca1d579a6a06283a570bf888f
Ronaldo Fidelis <ronaldofidelis.br@gmail.com>
2022-08-04 13:23:47 -0300

    Iniciando-projeto

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import MainContent from "./components/MainContent";
+import Sidebar from "./components/Sidebar";
+
+import "./style/components/app.sass";
+
+
+function App() {
+  return (
+    <div id="portifolio">
+      <h1>I'am Ronaldo</h1>
+      <Sidebar />
+      <MainContent />
+    </div>
+  );
+}
+
+export default App;
