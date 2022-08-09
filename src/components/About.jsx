c71c0cdce30bdaac9765ad5ed31ffa83d92146cf
Ronaldo Fidelis <ronaldofidelis.br@gmail.com>
2022-08-09 14:37:38 -0300

    atestes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
     <section className="about-container">
         <h2>Sobre</h2>
         <p>
-            Sou programador, e estou em buscar da primeira oportunidade na área de desenvolvimento front-end. Atualmente estou cursando faculdade de sistemas para internet. Também faço parte do projeto Embarque Digital, organizada e custeado pela Prefeitura do Recife e o parque tecnologico Porto Digital.
+            Meu nome é Ronaldo Rafael, e estou em buscar da primeira oportunidade na área de desenvolvimento front-end. Atualmente estou cursando faculdade de sistemas para internet. Também faço parte do projeto Embarque Digital, organizada e custeado pela Prefeitura do Recife e o parque tecnologico Porto Digital.
         </p>
         <p>
             Por meio do programa, tive a oportunidade de presenciar e participar de projetos com empresas e instituições parceiras, visando alcança soluções tecnologicas para problemas proposto pelas mesmas. Tenho conhecimento em JavaScript, Html5, Css3, NodeJs, versionamento de codigo com o Git e alguns frameworks e bibliotecas, como: React e Sass.
--- src/components/Tecnologies.jsx
+++ src/components/Tecnologies.jsx
@@ -5,24 +5,28 @@ import {
   DiNodejsSmall,
   DiReact,
   DiGit,
-  DiJavascript
+  DiJava,
+  DiJavascript,
+  DiSass
 } from "react-icons/di";
 
 import '../style/components/tecnologies.sass'
 
 //Objetos com o icons
 const IconsTecnologies = [
-  {id:"js", name:"JavaScript", icon: <DiJavascript/>, discription: ""},
+  {id:"js", name:"JavaScript", icon: <DiJavascript/>, discription: "É uma linguagem de programação. Juntamente com HTML e CSS, é uma das três principais tecnologias da web."},
   {id:"react", name:"ReactJS", icon: <DiReact/>, discription: ""},
-  {id:"html", name:"Html5", icon: <DiHtml5/>, discription: ""},
+  {id:"html", name:"Html5", icon: <DiHtml5/>, discription: "É uma linguagem de marcação, utilizada na construção de paginas na Web."},
+  {id:"java", name:"Java", icon: <DiJava/>, discription: ""},
   {id:"css", name:"Css3", icon: <DiCss3/>, discription: ""},
   {id:"node", name:"NodeJs", icon: <DiNodejsSmall/>, discription:""},
-  {id:"git", name:"Git", icon: <DiGit/>}
+  {id:"git", name:"Git", icon: <DiGit/>},
+  {id:"sass", name:"Sass", icon: <DiSass/>}
 ];
 
 const Tecnologies = () => {
   return <section className='tecnologies-container'>
-          <h2>Tecnologias</h2>
+          <h2>Habilidades</h2>
           <div className="tecnologies-grid">
             {IconsTecnologies.map((tech) => (
               <div className="tech-card" id={tech.id} key={tech.id}>
