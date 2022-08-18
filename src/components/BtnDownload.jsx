f9420c9f449b07caa797627574f5f770b685c807
Ronaldo Fidelis <ronaldofidelis.br@gmail.com>
2022-08-18 10:16:37 -0300

    teste

--- src/components/BtnDownload.jsx
+++ src/components/BtnDownload.jsx
@@ -1,13 +1,13 @@
 
 const btn = [
-    {a:"src/files/Curiculo - Ronaldo Rafael Fidelis Da Silva.zip"}
+    {a:"../src/files/Curiculo - Ronaldo Rafael Fidelis Da Silva.pdf", down:'Curriculo'}
 ];
 
 function BtnDownload() {
   return (
     <section>
         {btn.map((curriculo) =>(
-            <a href={curriculo.a} className="btn" download={'Curriculo'}>
+            <a href={curriculo.a} className="btn" download={curriculo.down}>
                 Download Curriculo
           </a>
         ))};
