e11c1e683e12f20840bba471a84d009de5fb5f16
RafaelFidelis <ronaldofidelis.ti@gmail.com>
2022-09-26 07:17:19 -0300

    attAbout

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,13 +3,11 @@
 const About = () => {
   return (
     <section className="about-container">
-        <h2>Sobre</h2>
-        <p>
-            Meu nome é Ronaldo Rafael, tenho 22 anos e sou apaixonado por tecnologia. Estou buscando oportunidade na área de desenvolvimento front-end, e atualmente estou cursando faculdade de sistemas para internet. Também faço parte do projeto Embarque Digital, organizada e custeado pela Prefeitura do Recife e o parque tecnologico Porto Digital.
-        </p>
-        <p>
-            Por meio do programa, tive oportunidade de aprender e participar de projetos com empresas e instituições parceiras, visando alcança soluções tecnologicas para problemas proposto, além de promover a formação técnica na área de tecnologia. Tenho conhecimento em JavaScript, Html5, Css3, NodeJs, Java, versionamento de codigo com o Git e alguns frameworks e bibliotecas, como: React e Sass.
-        </p>
+      <h2>Sobre</h2>
+      <p>
+        Atualmente sou estudante de engenharia de software e entusiasta pela área tecnologia, principalmente por desenvolvimento de sistemas web. Fui imerso por assuntos relacionados a programação, a cerca de um ano e meio, e desde então venho me empenhado a estudar sobre linguagem e ferramentas voltadas para o desenvolvimento Front-End.        </p>
+      <p>
+        Tive oportunidades de participar de projetos do Porto Digital, onde tive a experiência de desenvolver aplicações web para empresas, utilizando metodologias ágeis, design UI/UX e alguns conceitos de gestão de projeto, além de desenvolver o projeto utilizando linguagem de programação.         </p>
     </section>
   )
 }
